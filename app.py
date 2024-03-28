dae5eed9c2539657f0d35880757002bf24d08740
Shubhankar Kaushik <shubhankar.kaushik2003@gmail.com>
2024-03-28 21:31:25 +0530

    Added Decryption

--- app.py
+++ app.py
@@ -13,9 +13,21 @@ def encrypt(code_in):
 
 
 def decrypt(code_in):
-    pass
-
-def converter():
-    pass
-
-encrypt('Hello')
+    code_in += ' '
+    decrypted_msg = ''
+    morse_char = ''
+    inv_morse_dict = {v: k for k, v in mcd.items()}
+    space_count = 0
+    for char in code_in:
+        if char != ' ':
+            morse_char += char
+            space_count = 0
+        else:
+            space_count += 1
+            if space_count == 2:
+                decrypted_msg += ' '
+            else:
+                decrypted_msg += inv_morse_dict.get(morse_char, '')
+                morse_char = ''
+    
+    return decrypted_msg
